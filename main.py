69b19ededacb640f8c0efdb5dab764e2d77e57f0
hiddenbebb <cbj4133@naver.com>
2024-08-23 03:11:55 +0900

    add: 정각 정상 작동 알림 추가

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import asyncio
 from tel_func import TelegramForwarder
+from datetime import datetime
+
+async def send_hourly_message(forwarder, destination_channel_id):
+    while True:
+        now = datetime.now()
+        # 정각 확인
+        if now.minute == 0 and now.second == 0:
+            await forwarder.client.send_message(destination_channel_id, "정상작동 중")
+            print("정각 메시지 전송됨")
+            # 60초 동안 대기하여 동일한 정각에서 메시지가 여러 번 전송되는 것을 방지
+            await asyncio.sleep(60)
+        await asyncio.sleep(1)  # 매 초마다 확인
 
 
 async def main():
@@ -23,6 +35,8 @@ async def main():
     source_chat_id3= 7438319188 #coinnews
     last_message_id3 = None
     destination_channel_id=-1002203481397 #ㄱㄴㄷ
+
+    asyncio.create_task(send_hourly_message(forwarder, destination_channel_id))
 
     while True:
         last_message_id1 = await forwarder.forward_messages_to_channel(source_chat_id1, destination_channel_id, '', last_message_id1)
