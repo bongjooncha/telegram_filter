e10a98cdef3d047b65fd3b8505d7470bf020658a
bongjooncha <cbj4133@naver.com>
2024-10-14 17:33:52 +0900

    방법 찾아나섬

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 import asyncio
 from tel_func import TelegramForwarder
-from datetime import datetime
+import time
 try:
     from chats_ids import chatIds
 except ImportError:
     exit()
 
-async def send_hourly_message(forwarder, destination_channel_id):
-    while True:
-        now = datetime.now()
-        # 정각 확인
-        if now.hour == 12 and now.minute == 0 and now.second == 0:
-            await forwarder.client.send_message(destination_channel_id, "정상작동 중")
-            print("정각 메시지 전송됨")
-            # 60초 동안 대기하여 동일한 정각에서 메시지가 여러 번 전송되는 것을 방지
-            await asyncio.sleep(60)
-        await asyncio.sleep(60)  # 매 초마다 확인
-
+async def process_chat(forwarder, chat_id, destination_channel_id, keywords, last_message_id):
+    try:
+        return await forwarder.forward_messages_to_channel(chat_id, destination_channel_id, keywords, last_message_id)
+    except ValueError as e:
+        await forwarder.client.send_message(destination_channel_id, "텔레그램 오류 재시작")
+        await forwarder.send_error_message(destination_channel_id, str(e))
+        return last_message_id
 
 async def main():
     forwarder = TelegramForwarder()
     await forwarder.client.connect()
     
-    # 채널 갱신
-    # await forwarder.list_chats()
     
     # #auth 확인
     if not await forwarder.client.is_user_authorized():
@@ -36,20 +30,22 @@ async def main():
     last_message_id = [None]*len(chatIds)
     destination_channel_id = -1002158655628
     await forwarder.client.send_message(destination_channel_id, "시작")
-    asyncio.create_task(send_hourly_message(forwarder, destination_channel_id))
+    print("시작")
 
     while True:
-        for i in range(len(chatIds)):
-            if chat_ids_list[i] != destination_channel_id:
-                try:
-                    last_message_id[i] = await forwarder.forward_messages_to_channel(chat_ids_list[i],
-                                                                                 destination_channel_id, 
-                                                                             ['실리콘투','화장품','뷰티'], 
-                                                                                 last_message_id[i])
-                except ValueError as e:
-                    await forwarder.client.send_message(destination_channel_id, "텔레그램 오류 재시작")
-                    await forwarder.send_error_message(destination_channel_id,str(e))
-        print(1)
+            start_time = time.time()
+            tasks = [
+                process_chat(forwarder, chat_id, destination_channel_id, ['실리콘투','화장품','뷰티'], last_message_id[i])
+                for i, chat_id in enumerate(chat_ids_list) if chat_id != destination_channel_id
+            ]
+            results = await asyncio.gather(*tasks)
+            for i, result in enumerate(results):
+                last_message_id[i] = result
+            
+            end_time = time.time()
+            execution_time = end_time - start_time
+            print(f"실행 시간: {execution_time:.2f}초")
+            await forwarder.client.send_message(destination_channel_id, f"실행 시간: {execution_time:.2f}초")
     # 전체 메시지 호출()
     # messages = await forwarder.fetch_all_messages(source_chat_ids,2)
     # for msg in messages:
